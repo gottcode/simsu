d477a791e0670a574d6ff08b858f0bc555c1775b
Graeme Gott <graeme@gottcode.org>
2012-06-18 18:52:31 -0400

    Update translations.

--- translations/simsu_ca.ts
+++ translations/simsu_ca.ts
@@ -94,205 +94,205 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="../src/window.cpp" line="74"/>
+        <location filename="../src/window.cpp" line="71"/>
         <source>Simsu</source>
         <translation>Simsu</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="92"/>
+        <location filename="../src/window.cpp" line="89"/>
         <source>Pen</source>
         <translation>Bolígraf</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="96"/>
+        <location filename="../src/window.cpp" line="93"/>
         <source>Pencil</source>
         <translation>Llapis</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="100"/>
+        <location filename="../src/window.cpp" line="97"/>
         <source>Highlight</source>
         <translation>Marcador</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="102"/>
+        <location filename="../src/window.cpp" line="99"/>
         <source>H</source>
         <translation>H</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="109"/>
+        <location filename="../src/window.cpp" line="106"/>
         <source>S</source>
         <translation>S</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="146"/>
+        <location filename="../src/window.cpp" line="143"/>
         <source>&amp;Game</source>
         <translation>&amp;Joc</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="147"/>
+        <location filename="../src/window.cpp" line="144"/>
         <source>&amp;New</source>
         <translation>&amp;Nou</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="147"/>
+        <location filename="../src/window.cpp" line="144"/>
         <source>Ctrl+N</source>
         <translation>Ctrl+N</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="148"/>
+        <location filename="../src/window.cpp" line="145"/>
         <source>&amp;Details</source>
         <translation>&amp;Detalls</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="150"/>
+        <location filename="../src/window.cpp" line="147"/>
         <source>&amp;Quit</source>
         <translation>&amp;Surt</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="150"/>
+        <location filename="../src/window.cpp" line="147"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="152"/>
+        <location filename="../src/window.cpp" line="149"/>
         <source>&amp;Move</source>
         <translation>&amp;Mou</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="153"/>
+        <location filename="../src/window.cpp" line="150"/>
         <source>&amp;Undo</source>
         <translation>&amp;Desfés</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="153"/>
+        <location filename="../src/window.cpp" line="150"/>
         <source>Ctrl+Z</source>
         <translation>Ctrl+Z</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="155"/>
+        <location filename="../src/window.cpp" line="152"/>
         <source>&amp;Redo</source>
         <translation>&amp;Refés</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="155"/>
+        <location filename="../src/window.cpp" line="152"/>
         <source>Shift+Ctrl+Z</source>
         <translation>Majús+Ctrl+Z</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="158"/>
+        <location filename="../src/window.cpp" line="155"/>
         <source>&amp;Check</source>
         <translation>&amp;Comprova</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="158"/>
+        <location filename="../src/window.cpp" line="155"/>
         <source>C</source>
         <translation>C</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="161"/>
+        <location filename="../src/window.cpp" line="158"/>
         <source>&amp;Settings</source>
         <translation>&amp;Configuració</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="162"/>
+        <location filename="../src/window.cpp" line="159"/>
         <source>&amp;Auto Switch Modes</source>
         <translation>&amp;Modes d&apos;alternança automàtica</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="166"/>
+        <location filename="../src/window.cpp" line="163"/>
         <source>&amp;Widescreen Layout</source>
         <translation>&amp;Pla en apaisat</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="171"/>
+        <location filename="../src/window.cpp" line="168"/>
         <source>Application &amp;Language...</source>
         <translation>&amp;Idioma...</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="173"/>
-        <location filename="../src/window.cpp" line="180"/>
+        <location filename="../src/window.cpp" line="170"/>
+        <location filename="../src/window.cpp" line="177"/>
         <source>&amp;Help</source>
         <translation>&amp;Ajuda</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="174"/>
+        <location filename="../src/window.cpp" line="171"/>
         <source>&amp;Controls</source>
         <translation>&amp;Controls</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="182"/>
+        <location filename="../src/window.cpp" line="179"/>
         <source>&amp;About</source>
         <translation>&amp;Quant a...</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="183"/>
+        <location filename="../src/window.cpp" line="180"/>
         <source>About &amp;Qt</source>
         <translation>Quant a &amp;Qt</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="221"/>
+        <location filename="../src/window.cpp" line="218"/>
         <source>New Game</source>
         <translation>Nou joc</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="238"/>
-        <location filename="../src/window.cpp" line="278"/>
+        <location filename="../src/window.cpp" line="235"/>
+        <location filename="../src/window.cpp" line="275"/>
         <source>Dancing Links</source>
         <translation>Dancing Links</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="239"/>
-        <location filename="../src/window.cpp" line="278"/>
+        <location filename="../src/window.cpp" line="236"/>
+        <location filename="../src/window.cpp" line="275"/>
         <source>Slice and Dice</source>
         <translation>Talla a trossets</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="244"/>
+        <location filename="../src/window.cpp" line="241"/>
         <source>Random</source>
         <translation>Aleatori</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="252"/>
+        <location filename="../src/window.cpp" line="249"/>
         <source>Symmetry:</source>
         <translation>Simetria:</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="253"/>
+        <location filename="../src/window.cpp" line="250"/>
         <source>Algorithm:</source>
         <translation>Algoritme:</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="254"/>
+        <location filename="../src/window.cpp" line="251"/>
         <source>Seed:</source>
         <translation>Llavor:</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="281"/>
+        <location filename="../src/window.cpp" line="278"/>
         <source>Details</source>
         <translation>Detalls</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="281"/>
+        <location filename="../src/window.cpp" line="278"/>
         <source>&lt;p&gt;&lt;b&gt;Symmetry:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %L2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
         <translation>&lt;p&gt;&lt;b&gt;Simetria:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Algoritme:&lt;/b&gt; %L2&lt;br&gt;&lt;b&gt;Llavor:&lt;/b&gt; %L3&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="289"/>
+        <location filename="../src/window.cpp" line="286"/>
         <source>Controls</source>
         <translation>Controls</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="289"/>
+        <location filename="../src/window.cpp" line="286"/>
         <source>&lt;p&gt;&lt;big&gt;&lt;b&gt;Mouse Controls:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Left click:&lt;/b&gt; Toggle number in pen mode&lt;br&gt;&lt;b&gt;Right click:&lt;/b&gt; Toggle number in pencil mode&lt;br&gt;&lt;b&gt;Scrollwheel:&lt;/b&gt; Change current number&lt;/p&gt;&lt;p&gt;&lt;big&gt;&lt;b&gt;Keyboard Controls:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Arrow keys:&lt;/b&gt; Move selection&lt;br&gt;&lt;b&gt;Number keys:&lt;/b&gt; Toggle value or note&lt;br&gt;&lt;b&gt;S:&lt;/b&gt; Switch between pen and pencil modes&lt;br&gt;&lt;b&gt;H:&lt;/b&gt; Highlight all instances of current number&lt;/p&gt;</source>
         <translation>&lt;p&gt;&lt;big&gt;&lt;b&gt;Controls del ratolí:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Clic esquerre:&lt;/b&gt; Activa nombre en mode bolígraf&lt;br&gt;&lt;b&gt;Clic dret:&lt;/b&gt; Activa nombre en mode llapis&lt;br&gt;&lt;b&gt;Rodeta:&lt;/b&gt; Canvia el nombre actual&lt;/p&gt;&lt;p&gt;&lt;big&gt;&lt;b&gt;Controls del teclat:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Fletxes de direcció:&lt;/b&gt; Mou la selecció&lt;br&gt;&lt;b&gt;tecles numèriques:&lt;/b&gt; Activa valor o nota&lt;br&gt;&lt;b&gt;S:&lt;/b&gt; Alterna entre modes bolígraf i llapis&lt;br&gt;&lt;b&gt;H:&lt;/b&gt; Subratlla tots els exemples de nombre actual&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="303"/>
+        <location filename="../src/window.cpp" line="300"/>
         <source>About Simsu</source>
         <translation>Quant a Simsu</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="303"/>
+        <location filename="../src/window.cpp" line="300"/>
         <source>&lt;center&gt;&lt;big&gt;&lt;b&gt;Simsu %1&lt;/b&gt;&lt;/big&gt;&lt;br/&gt;A basic Sudoku game&lt;br/&gt;&lt;small&gt;Copyright &amp;copy; 2009-2012 Graeme Gott&lt;/small&gt;&lt;/center&gt;</source>
         <translation>&lt;center&gt;&lt;big&gt;&lt;b&gt;Simsu %1&lt;/b&gt;&lt;/big&gt;&lt;br/&gt;Un joc de Sudoku senzill&lt;br/&gt;&lt;small&gt;Copyright &amp;copy; 2009-2012 Graeme Gott&lt;/small&gt;&lt;/center&gt;</translation>
     </message>
--- translations/simsu_el.ts
+++ translations/simsu_el.ts
@@ -94,205 +94,205 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="../src/window.cpp" line="74"/>
+        <location filename="../src/window.cpp" line="71"/>
         <source>Simsu</source>
         <translation>Simsu</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="92"/>
+        <location filename="../src/window.cpp" line="89"/>
         <source>Pen</source>
         <translation>Στυλό</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="96"/>
+        <location filename="../src/window.cpp" line="93"/>
         <source>Pencil</source>
         <translation>Μολύβι</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="100"/>
+        <location filename="../src/window.cpp" line="97"/>
         <source>Highlight</source>
         <translation>Επισήμανση</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="102"/>
+        <location filename="../src/window.cpp" line="99"/>
         <source>H</source>
         <translation>H</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="109"/>
+        <location filename="../src/window.cpp" line="106"/>
         <source>S</source>
         <translation>S</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="146"/>
+        <location filename="../src/window.cpp" line="143"/>
         <source>&amp;Game</source>
         <translation>&amp;Παρτίδα</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="147"/>
+        <location filename="../src/window.cpp" line="144"/>
         <source>&amp;New</source>
         <translation>&amp;Νέα</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="147"/>
+        <location filename="../src/window.cpp" line="144"/>
         <source>Ctrl+N</source>
         <translation>Ctrl+N</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="148"/>
+        <location filename="../src/window.cpp" line="145"/>
         <source>&amp;Details</source>
         <translation>&amp;Λεπτομέρειες</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="150"/>
+        <location filename="../src/window.cpp" line="147"/>
         <source>&amp;Quit</source>
         <translation>&amp;Έξοδος</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="150"/>
+        <location filename="../src/window.cpp" line="147"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="152"/>
+        <location filename="../src/window.cpp" line="149"/>
         <source>&amp;Move</source>
         <translation>&amp;Κίνηση</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="153"/>
+        <location filename="../src/window.cpp" line="150"/>
         <source>&amp;Undo</source>
         <translation>&amp;Αναίρεση</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="153"/>
+        <location filename="../src/window.cpp" line="150"/>
         <source>Ctrl+Z</source>
         <translation>Ctrl+Z</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="155"/>
+        <location filename="../src/window.cpp" line="152"/>
         <source>&amp;Redo</source>
         <translation>&amp;Επανάληψη</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="155"/>
+        <location filename="../src/window.cpp" line="152"/>
         <source>Shift+Ctrl+Z</source>
         <translation>Shift+Ctrl+Z</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="158"/>
+        <location filename="../src/window.cpp" line="155"/>
         <source>&amp;Check</source>
         <translation>&amp;Έλεγχος</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="158"/>
+        <location filename="../src/window.cpp" line="155"/>
         <source>C</source>
         <translation>C</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="161"/>
+        <location filename="../src/window.cpp" line="158"/>
         <source>&amp;Settings</source>
         <translation>&amp;Ρυθμίσεις</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="162"/>
+        <location filename="../src/window.cpp" line="159"/>
         <source>&amp;Auto Switch Modes</source>
         <translation>%Αυτόματη Αλλαγή Λειτουργίας</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="166"/>
+        <location filename="../src/window.cpp" line="163"/>
         <source>&amp;Widescreen Layout</source>
         <translation>&amp;Προβολή Ευρύς Οθόνης</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="171"/>
+        <location filename="../src/window.cpp" line="168"/>
         <source>Application &amp;Language...</source>
         <translation>&amp;Γλώσσα Εφαρμογής...</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="173"/>
-        <location filename="../src/window.cpp" line="180"/>
+        <location filename="../src/window.cpp" line="170"/>
+        <location filename="../src/window.cpp" line="177"/>
         <source>&amp;Help</source>
         <translation>&amp;Βοήθεια</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="174"/>
+        <location filename="../src/window.cpp" line="171"/>
         <source>&amp;Controls</source>
         <translation>&amp;Χειρισμός</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="182"/>
+        <location filename="../src/window.cpp" line="179"/>
         <source>&amp;About</source>
         <translation>&amp;Σχετικά</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="183"/>
+        <location filename="../src/window.cpp" line="180"/>
         <source>About &amp;Qt</source>
         <translation>Σχετικά με το &amp;Qt</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="221"/>
+        <location filename="../src/window.cpp" line="218"/>
         <source>New Game</source>
         <translation>Νέο Παιχνίδι</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="238"/>
-        <location filename="../src/window.cpp" line="278"/>
+        <location filename="../src/window.cpp" line="235"/>
+        <location filename="../src/window.cpp" line="275"/>
         <source>Dancing Links</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="239"/>
-        <location filename="../src/window.cpp" line="278"/>
+        <location filename="../src/window.cpp" line="236"/>
+        <location filename="../src/window.cpp" line="275"/>
         <source>Slice and Dice</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="244"/>
+        <location filename="../src/window.cpp" line="241"/>
         <source>Random</source>
         <translation>Τυχαία</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="252"/>
+        <location filename="../src/window.cpp" line="249"/>
         <source>Symmetry:</source>
         <translation>Συμμετρία</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="253"/>
+        <location filename="../src/window.cpp" line="250"/>
         <source>Algorithm:</source>
         <translation>Αλγόρυθμος</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="254"/>
+        <location filename="../src/window.cpp" line="251"/>
         <source>Seed:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="281"/>
+        <location filename="../src/window.cpp" line="278"/>
         <source>Details</source>
         <translation>Λεπτομέριες</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="281"/>
+        <location filename="../src/window.cpp" line="278"/>
         <source>&lt;p&gt;&lt;b&gt;Symmetry:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %L2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="289"/>
+        <location filename="../src/window.cpp" line="286"/>
         <source>Controls</source>
         <translation>Χειρισμός</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="289"/>
+        <location filename="../src/window.cpp" line="286"/>
         <source>&lt;p&gt;&lt;big&gt;&lt;b&gt;Mouse Controls:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Left click:&lt;/b&gt; Toggle number in pen mode&lt;br&gt;&lt;b&gt;Right click:&lt;/b&gt; Toggle number in pencil mode&lt;br&gt;&lt;b&gt;Scrollwheel:&lt;/b&gt; Change current number&lt;/p&gt;&lt;p&gt;&lt;big&gt;&lt;b&gt;Keyboard Controls:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Arrow keys:&lt;/b&gt; Move selection&lt;br&gt;&lt;b&gt;Number keys:&lt;/b&gt; Toggle value or note&lt;br&gt;&lt;b&gt;S:&lt;/b&gt; Switch between pen and pencil modes&lt;br&gt;&lt;b&gt;H:&lt;/b&gt; Highlight all instances of current number&lt;/p&gt;</source>
         <translation>&lt;p&gt;&lt;big&gt;&lt;b&gt;Χειρισμός Ποντικιού:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Αριστερό Κλικ:&lt;/b&gt; Εναλλαγή αριθμού σε λειτουργία στυλού&lt;br&gt;&lt;b&gt;Δεξί κλικ:&lt;/b&gt; Εναλλαγή αριθμού σε λειτουργία μολιβιού&lt;br&gt;&lt;b&gt;Τροχός:&lt;/b&gt; Αλλαγή επιλεγμένου αριθμού&lt;/p&gt;&lt;p&gt;&lt;big&gt;&lt;b&gt;Χειρισμός Πληκτρολογίου:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Βελάκια:&lt;/b&gt; Μετακίνηση επιλογής&lt;br&gt;&lt;b&gt;Αριθμητικά πλήκτρα:&lt;/b&gt; Αλλαγή αριθμού&lt;br&gt;&lt;b&gt;S:&lt;/b&gt; Εναλλαγή αναμεσά λειτουργίες στυλού και μολυβιού&lt;br&gt;&lt;b&gt;H:&lt;/b&gt; Επισήμανση όλων των υποδείξεων του επιλεγμένου αριθμού&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="303"/>
+        <location filename="../src/window.cpp" line="300"/>
         <source>About Simsu</source>
         <translation>Σχετικά με το Simsu</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="303"/>
+        <location filename="../src/window.cpp" line="300"/>
         <source>&lt;center&gt;&lt;big&gt;&lt;b&gt;Simsu %1&lt;/b&gt;&lt;/big&gt;&lt;br/&gt;A basic Sudoku game&lt;br/&gt;&lt;small&gt;Copyright &amp;copy; 2009-2012 Graeme Gott&lt;/small&gt;&lt;/center&gt;</source>
         <translation>&lt;center&gt;&lt;big&gt;&lt;b&gt;Simsu %1&lt;/b&gt;&lt;/big&gt;&lt;br/&gt;Ένα βασικό παιχνίδι Σουντόκου&lt;br/&gt;&lt;small&gt;Copyright &amp;copy; 2009-2012 Graeme Gott&lt;/small&gt;&lt;/center&gt;</translation>
     </message>
