2f85e2ff5deddc81af68086f799ae0ad9224d367
Graeme Gott <graeme@gottcode.org>
2014-11-14 15:55:31 -0500

    Update appdata file.

--- icons/po/update-po.sh
+++ icons/po/update-po.sh
@@ -21,10 +21,13 @@ rm -f simsu.appdata.xml.in
 cp simsu.appdata.xml simsu.appdata.xml.in
 sed -e '/p xml:lang/ d' \
 	-e '/summary xml:lang/ d' \
+	-e '/name xml:lang/ d' \
 	-e 's/<p>/<_p>/' \
 	-e 's/<\/p>/<\/_p>/' \
 	-e 's/<summary>/<_summary>/' \
 	-e 's/<\/summary>/<\/_summary>/' \
+	-e 's/<name>/<_name>/' \
+	-e 's/<\/name>/<\/_name>/' \
 	-i simsu.appdata.xml.in
 intltool-extract --quiet --type=gettext/xml simsu.appdata.xml.in
 
