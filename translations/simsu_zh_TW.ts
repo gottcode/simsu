5f5d007f99e484b9bd94835cd5929059a56af56a
Graeme Gott <graeme@gottcode.org>
2017-08-05 13:36:27 -0400

    Sync translations with Transifex.

---
+++ translations/simsu_zh_TW.ts
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW" sourcelanguage="en">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Success</source>
+        <translation>成功</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>選擇本程式的語言</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;系統語言&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>劃記</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>請重新啟動程式，變更成為你指定的語言</translation>
+    </message>
+</context>
+<context>
+    <name>Pattern</name>
+    <message>
+        <source>180° Rotational</source>
+        <translation>180度旋轉</translation>
+    </message>
+    <message>
+        <source>Full Rotational</source>
+        <translation>全翻轉</translation>
+    </message>
+    <message>
+        <source>Horizontal</source>
+        <translation>水平翻轉</translation>
+    </message>
+    <message>
+        <source>Vertical</source>
+        <translation>垂直翻轉</translation>
+    </message>
+    <message>
+        <source>Horizontal &amp; Vertical</source>
+        <translation>水平暨垂直翻轉</translation>
+    </message>
+    <message>
+        <source>Diagonal</source>
+        <translation>正對角線</translation>
+    </message>
+    <message>
+        <source>Anti-Diagonal</source>
+        <translation>反對角線</translation>
+    </message>
+    <message>
+        <source>Diagonal &amp; Anti-Diagonal</source>
+        <translation>正對角線線與反對角線</translation>
+    </message>
+    <message>
+        <source>Full Dihedral</source>
+        <translation>全二面角</translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>任意</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>沒有</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Simsu</source>
+        <translation>Simsu</translation>
+    </message>
+    <message>
+        <source>Pen</source>
+        <translation>原子筆</translation>
+    </message>
+    <message>
+        <source>Pencil</source>
+        <translation>鉛筆</translation>
+    </message>
+    <message>
+        <source>Highlight</source>
+        <translation>高亮色標示</translation>
+    </message>
+    <message>
+        <source>H</source>
+        <translation>H</translation>
+    </message>
+    <message>
+        <source>S</source>
+        <translation>S</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Game</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;New</translation>
+    </message>
+    <message>
+        <source>&amp;Details</source>
+        <translation>&amp;Details</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quit</translation>
+    </message>
+    <message>
+        <source>&amp;Move</source>
+        <translation>&amp;Move</translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;Undo</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>&amp;Redo</translation>
+    </message>
+    <message>
+        <source>&amp;Check</source>
+        <translation>&amp;Check</translation>
+    </message>
+    <message>
+        <source>C</source>
+        <translation>C</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Settings</translation>
+    </message>
+    <message>
+        <source>&amp;Auto Switch Modes</source>
+        <translation>&amp;Auto Switch Modes</translation>
+    </message>
+    <message>
+        <source>&amp;Widescreen Layout</source>
+        <translation>&amp;Widescreen Layout</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>應用程式 &amp;Language...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message>
+        <source>&amp;Controls</source>
+        <translation>&amp;Controls</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;About</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>關於　&amp;Qt</translation>
+    </message>
+    <message>
+        <source>New Game</source>
+        <translation>新遊戲</translation>
+    </message>
+    <message>
+        <source>Dancing Links</source>
+        <translation>舞動連結</translation>
+    </message>
+    <message>
+        <source>Slice and Dice</source>
+        <translation>切片且投骰子</translation>
+    </message>
+    <message>
+        <source>Random</source>
+        <translation>任意產生</translation>
+    </message>
+    <message>
+        <source>Symmetry:</source>
+        <translation>對稱</translation>
+    </message>
+    <message>
+        <source>Algorithm:</source>
+        <translation>演算法</translation>
+    </message>
+    <message>
+        <source>Seed:</source>
+        <translation>種子</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>詳細細節</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;b&gt;Symmetry:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;Algorithm:&lt;/b&gt; %L2&lt;br&gt;&lt;b&gt;Seed:&lt;/b&gt; %L3&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;b&gt;對稱:&lt;/b&gt; %1&lt;br&gt;&lt;b&gt;演算法:&lt;/b&gt; %L2&lt;br&gt;&lt;b&gt;種子:&lt;/b&gt; %L3&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Controls</source>
+        <translation>控制</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;&lt;big&gt;&lt;b&gt;Mouse Controls:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Left click:&lt;/b&gt; Toggle number in pen mode&lt;br&gt;&lt;b&gt;Right click:&lt;/b&gt; Toggle number in pencil mode&lt;br&gt;&lt;b&gt;Scrollwheel:&lt;/b&gt; Change current number&lt;/p&gt;&lt;p&gt;&lt;big&gt;&lt;b&gt;Keyboard Controls:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;Arrow keys:&lt;/b&gt; Move selection&lt;br&gt;&lt;b&gt;Number keys:&lt;/b&gt; Toggle value or note&lt;br&gt;&lt;b&gt;S:&lt;/b&gt; Switch between pen and pencil modes&lt;br&gt;&lt;b&gt;H:&lt;/b&gt; Highlight all instances of current number&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;big&gt;&lt;b&gt;滑鼠控制:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;左鍵點擊:&lt;/b&gt; 切換原子筆模式的數字&lt;br&gt;&lt;b&gt;右鍵點擊:&lt;/b&gt; 切換鉛筆模式的數字&lt;br&gt;&lt;b&gt;滾輪:&lt;/b&gt; 改變現在的數字&lt;/p&gt;&lt;p&gt;&lt;big&gt;&lt;b&gt;鍵盤控制:&lt;/b&gt;&lt;/big&gt;&lt;br&gt;&lt;b&gt;方向建:&lt;/b&gt; 移動選擇&lt;br&gt;&lt;b&gt;數字鍵:&lt;/b&gt; 切換數值或註記&lt;br&gt;&lt;b&gt;S:&lt;/b&gt; 切換原子筆模式或鉛筆模式&lt;br&gt;&lt;b&gt;H:&lt;/b&gt; 把目前的數字，在所有盤格裡都以高亮色標示&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>About Simsu</source>
+        <translation>關於Simsu</translation>
+    </message>
+    <message>
+        <source>A basic Sudoku game</source>
+        <translation>一個基本的數獨遊戲</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation>版權　&amp;copy; 2009-%1 Graeme Gott</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>這版本以 &lt;a href=%1&gt;GPL 3&lt;/a&gt;授權</translation>
+    </message>
+</context>
+</TS>
