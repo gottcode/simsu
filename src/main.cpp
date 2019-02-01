c36bd1305613b360fcdad15e6c5323f06de33da4
Graeme Gott <graeme@gottcode.org>
2019-02-01 08:45:12 -0500

    FIXED: Window icon didn't work in Wayland.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2018 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2018, 2019 Graeme Gott <graeme@gottcode.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -47,6 +47,7 @@ int main(int argc, char** argv)
 	app.setOrganizationName("GottCode");
 #if !defined(Q_OS_WIN) && !defined(Q_OS_MAC)
 	app.setWindowIcon(QIcon::fromTheme("simsu", QIcon(":/simsu.png")));
+	app.setDesktopFileName("simsu");
 #endif
 	app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 
